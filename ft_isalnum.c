82bccfa728ab32745488531e7416cbb46ad8bddd
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-04-02 23:58:02 +0200

    feat: ft_isalnum finished and fix bugs in tests

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,21 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/02 17:03:47 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/04/02 23:48:49 by eandre-f         ###   ########.fr       */
+/*   Created: 2022/04/02 23:44:18 by eandre-f          #+#    #+#             */
+/*   Updated: 2022/04/02 23:46:57 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-// Part 1
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-
-#endif
+int	ft_isalnum(int c)
+{
+	if (ft_isalpha(c) != 0 || ft_isdigit(c) != 0)
+		return (1);
+	else
+		return (0);
+}
--- tests.c
+++ tests.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 21:16:09 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/04/02 23:35:31 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/04/02 23:56:29 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,15 @@
 #include <stdio.h>
 #include <ctype.h>
 
-# define STATUS_SUCESS 0
-# define STATUS_ERROR 1
-# define MAX_TESTS 100
-# define FUNC_TEST(condition) (condition ? STATUS_SUCESS : STATUS_ERROR)
+#define STATUS_SUCESS 0
+#define STATUS_ERROR 1
+#define MAX_TESTS 100
+#define FUNC_TEST(condition) (condition ? STATUS_SUCESS : STATUS_ERROR)
 
-typedef struct t_test {
-	char *name;
-	int	list[MAX_TESTS];
-	int	status;
-} t_test;
-
-void	ft_test(t_test test_func, int ntests)
+void	ft_test(char *name_test, char *list_test, int ntests)
 {
-	int	i;
-	int status;
-	char *name_test = test_func.name;
-	int *list_test = test_func.list;
+	int		i;
+	int		status;
 
 	status = STATUS_SUCESS;
 	i = 0;
@@ -39,37 +31,40 @@ void	ft_test(t_test test_func, int ntests)
 		if (list_test[i] == STATUS_ERROR)
 		{
 			status = STATUS_ERROR;
-			break;
+			break ;
 		}
 		i++;
 	}
-	if (status == STATUS_ERROR) {
-		printf("%s --- KO\n", name_test);
-		printf("Error Test %d\n", i);
-	} else {
+	if (status == STATUS_ERROR)
+		printf("%s --- KO\nError Test %d\n", name_test, i);
+	else
 		printf("%s OK\n", name_test);
-	}
 }
 
 int	main(void)
 {
-	// ft_isalpha
-	t_test test_isalpha;
-	test_isalpha.name = "ft_isalpha";
-	test_isalpha.list[0] = FUNC_TEST(ft_isalpha('a') != 0 && isalpha('a') != 0);
-	test_isalpha.list[1] = FUNC_TEST(ft_isalpha('A') != 0 && isalpha('A') != 0);
-	test_isalpha.list[2] = FUNC_TEST(ft_isalpha('z') != 0 && isalpha('z') != 0);
-	test_isalpha.list[3] = FUNC_TEST(ft_isalpha('Z') != 0 && isalpha('Z') != 0);
-	test_isalpha.list[4] = FUNC_TEST(ft_isalpha(1) == isalpha(1));
-	test_isalpha.list[5] = FUNC_TEST(ft_isalpha('\n') == isalpha('\n'));
-	ft_test(test_isalpha, 6);
-	// ft_isdigit
-	t_test test_isdigit;
-	test_isdigit.name = "ft_isdigit";
-	test_isdigit.list[0] = FUNC_TEST(ft_isdigit('\n') == isdigit('\n'));
-	test_isdigit.list[1] = FUNC_TEST(ft_isdigit(1) == isdigit(1));
-	test_isdigit.list[2] = FUNC_TEST(ft_isdigit('a') == isdigit('a'));
-	test_isdigit.list[3] = FUNC_TEST(ft_isdigit('1') != 0 && isdigit('1') != 0);
-	ft_test(test_isdigit, 4);
+	char	*name;
+	char	list[MAX_TESTS];
+	
+	name = "ft_isalpha";
+	list[0] = FUNC_TEST(ft_isalpha('a') != 0 && isalpha('a') != 0);
+	list[1] = FUNC_TEST(ft_isalpha('A') != 0 && isalpha('A') != 0);
+	list[2] = FUNC_TEST(ft_isalpha('z') != 0 && isalpha('z') != 0);
+	list[3] = FUNC_TEST(ft_isalpha('Z') != 0 && isalpha('Z') != 0);
+	list[4] = FUNC_TEST(ft_isalpha(1) == isalpha(1));
+	list[5] = FUNC_TEST(ft_isalpha('\n') == isalpha('\n'));
+	ft_test(name, list, 6);
+	name = "ft_isdigit";
+	list[0] = FUNC_TEST(ft_isdigit('\n') == isdigit('\n'));
+	list[1] = FUNC_TEST(ft_isdigit(1) == isdigit(1));
+	list[2] = FUNC_TEST(ft_isdigit('a') == isdigit('a'));
+	list[3] = FUNC_TEST(ft_isdigit('1') != 0 && isdigit('1') != 0);
+	ft_test(name, list, 4);
+	name = "ft_isalnum";
+	list[0] = FUNC_TEST(ft_isalnum('\n') == isalnum('\n'));
+	list[1] = FUNC_TEST(ft_isalnum(1) == isalnum(1));
+	list[2] = FUNC_TEST(ft_isalnum('a') != 0 && isalnum('a') != 0);
+	list[3] = FUNC_TEST(ft_isalnum('1') != 0 && isalnum('1') != 0);
+	ft_test(name, list, 4);
 	return (0);
 }
