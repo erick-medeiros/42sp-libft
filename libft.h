770cf8f67ee0838e1cbb910434c1f767677a2b08
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-04-04 15:09:21 +0200

    refactor: add description in libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 17:03:47 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/04/03 03:00:07 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/04/04 15:08:20 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,22 @@
 
 #include <stddef.h>
 
+// #include <ctype.h>
 // Part 1
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
 int		ft_isprint(int c);
+
+// #include <string.h>
+// Part 1
 size_t	ft_strlen(char *s);
 void	*ft_memset(void *s, int c, size_t n);
+
+
+// #include <strings.h>
+// Part 1
 void	ft_bzero(void *s, size_t n);
 
 #endif
