6efc67919e09dce3b8dbb8b337d028995b88f372
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-04-06 04:26:05 +0200

    fix: verify null-byte src

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 15:53:45 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/04/04 16:07:48 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/04/06 04:21:01 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	if (size > 0)
 	{
 		i = 0;
-		while (i < size - 1)
+		while (i < size - 1 && src[i] != '\0')
 		{
 			dst[i] = src[i];
 			i++;
