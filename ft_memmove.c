33b6e14ce83ef370f17036f73d580f963733cb0a
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-04-07 03:01:03 +0200

    fix: was no overlapping content

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 15:22:14 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/04/04 15:42:25 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/04/07 02:54:51 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*temporary_array;
-	size_t	i;
+	size_t			i;
+	unsigned char	*dest_uc;
+	unsigned char	*src_uc;
 
-	i = 0;
-	temporary_array = (char *)src;
-	i = 0;
-	while (i < n)
+	dest_uc = (unsigned char *)dest;
+	src_uc = (unsigned char *)src;
+	i = 1;
+	while (i <= n)
 	{
-		((char *)dest)[i] = ((char *)temporary_array)[i];
+		dest_uc[n - i] = src_uc[n - i];
 		i++;
 	}
 	return (dest);
