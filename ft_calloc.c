de1156d663ea3397f60bb8352bc30dc8059d1d82
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-04-12 00:44:19 +0200

    fix: calloc do not work with 0 size

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 20:55:10 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/04/05 21:50:40 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/04/12 00:40:53 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_calloc(size_t nelem, size_t elsize)
 	int		max;
 
 	max = 2147483647;
-	if (nelem == 0 || elsize == 0 || nelem > max / elsize)
+	if (nelem > max / elsize)
 		return (NULL);
 	p = malloc(nelem * elsize);
 	if (p == NULL)
