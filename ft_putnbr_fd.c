3d847af35f4d40ed452fc2cba5e7abea209bb950
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-04-11 04:41:30 +0200

    fix: not working ft_putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,38 +6,32 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 07:01:44 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/04/09 07:08:08 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/04/11 04:37:50 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <unistd.h>
-
-static int	ft_putnbr_fd_topositive(int n)
-{
-	if (n < 0)
-		return (n * -1);
-	return (n);
-}
 
 void	ft_putnbr_fd(int n, int fd)
 {
+	char	s[10];
 	int		i;
-	int		cpn;
-	char	s[12];
 
-	cpn = n;
-	s[11] = '\0';
-	i = 10;
-	while ((i != 0 && n != 0) || (i == 10 && n == 0))
+	if (n == 0)
+		return (ft_putchar_fd('0', fd));
+	if (n < 0)
+		ft_putchar_fd('-', fd);
+	s[9] = '\0';
+	i = 8;
+	while (n != 0)
 	{
-		s[i] = ft_putnbr_fd_topositive(n % 10) + '0';
+		s[i] = n % 10;
+		if (s[i] < 0)
+			s[i] = s[i] * -1;
+		s[i] = s[i] + '0';
 		n = n / 10;
 		i--;
 	}
-	if (cpn < 0)
-		s[i] = '-';
-	else
-		i++;
-	write(fd, &s[i], 12 - i);
+	i++;
+	ft_putstr_fd(&s[i], fd);
 }
