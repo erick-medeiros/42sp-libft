d74b63fef02aeed645d6437be3b9330e6a421e04
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-04-13 06:10:09 +0200

    fix: remove protection calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/05 20:55:10 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/04/12 00:40:53 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/04/13 06:06:51 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,7 @@
 void	*ft_calloc(size_t nelem, size_t elsize)
 {
 	void	*p;
-	int		max;
 
-	max = 2147483647;
-	if (nelem > max / elsize)
-		return (NULL);
 	p = malloc(nelem * elsize);
 	if (p == NULL)
 		return (NULL);
