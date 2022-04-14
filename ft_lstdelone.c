e951caa6d2745df0e2cd5000512ef70bb2a06ff6
Erick Medeiros <erickmedeirosbr@gmail.com>
2022-04-14 17:49:25 +0200

    refactor: ft_lstdelone

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,7 +6,7 @@
 /*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/09 20:18:46 by eandre-f          #+#    #+#             */
-/*   Updated: 2022/04/09 20:33:27 by eandre-f         ###   ########.fr       */
+/*   Updated: 2022/04/14 17:41:54 by eandre-f         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,8 @@
 
 void	ft_lstdelone(t_list *lst, void (*del)(void*))
 {
-	t_list	*node;
-
 	if (lst == NULL)
 		return ;
-	node = lst;
-	lst = lst->next;
-	del(node->content);
-	free(node);
+	del(lst->content);
+	free(lst);
 }
